fc6f2be8fd773d4875a113f1f746603e8eb7627e
Till <seebergertill@gmail.com>
2019-07-31 18:12:08 +0200

    refactor qos usage to match MqttAction commands

--- src/main/java/com/hivemq/cli/mqtt/AbstractMqttClientExecutor.java
+++ src/main/java/com/hivemq/cli/mqtt/AbstractMqttClientExecutor.java
@@ -20,25 +20,11 @@ import org.jetbrains.annotations.Nullable;
 import org.pmw.tinylog.Logger;
 import org.pmw.tinylog.LoggingContext;
 
-import java.nio.charset.StandardCharsets;
-
 abstract class AbstractMqttClientExecutor {
 
     private ClientCache<String, Mqtt5AsyncClient> clientCache = new ClientCache<>();
 
-    private static MqttQos getQosFromParamField(int[] qos, int i) {
-        if (qos == null || qos.length <= i) {
-            return MqttQos.AT_MOST_ONCE;
-        }
-        return getQosFromInt(qos[i]);
-    }
 
-    private static MqttQos getQosFromInt(int qos) {
-        if (qos == 0) {
-            return MqttQos.AT_MOST_ONCE;
-        }
-        return qos == 1 ? MqttQos.AT_LEAST_ONCE : MqttQos.EXACTLY_ONCE;
-    }
 
     abstract boolean mqttConnect(Mqtt5BlockingClient client, Mqtt5Connect connectMessage, Connect connectCommand);
 
@@ -133,7 +119,7 @@ abstract class AbstractMqttClientExecutor {
 
         for (int i = 0; i < subscribe.getTopics().length; i++) {
             final String topic = subscribe.getTopics()[i];
-            final MqttQos qos = getQosFromParamField(subscribe.getQos(), i);
+            final MqttQos qos = subscribe.getQos()[i];
 
             mqttSubscribe(client, subscribe, topic, qos);
 
@@ -146,7 +132,7 @@ abstract class AbstractMqttClientExecutor {
     private Mqtt5AsyncClient doPublish(Mqtt5AsyncClient client, final @NotNull Publish publish) {
         for (int i = 0; i < publish.getTopics().length; i++) {
             final String topic = publish.getTopics()[i];
-            final MqttQos qos = getQosFromParamField(publish.getQos(), i);
+            final MqttQos qos = publish.getQos()[i];
 
             client.publishWith()
                     .topic(topic)
@@ -178,11 +164,11 @@ abstract class AbstractMqttClientExecutor {
     private Mqtt5Publish createWillPublish(final @NotNull Connect connectCommand) throws Exception {
         // only topic is mandatory for will message creation
         if (connectCommand.getWillTopic() != null) {
-            byte[] willpayload = connectCommand.getWillMessage() != null ? connectCommand.getWillMessage().getBytes() : null;
+            byte[] willpayload = connectCommand.getWillMessage() != null ? connectCommand.getWillMessage() : null;
             Mqtt5WillPublishBuilder builder = Mqtt5WillPublish.builder()
                     .topic(connectCommand.getWillTopic())
                     .payload(willpayload)
-                    .qos(getQosFromInt(connectCommand.getWillQos()))
+                    .qos(connectCommand.getWillQos())
                     .retain(connectCommand.isWillRetain());
             try {
                 return ((Mqtt5WillPublishBuilder.Complete) builder).build().asWill();
@@ -190,7 +176,7 @@ abstract class AbstractMqttClientExecutor {
                 Logger.error("Client can't create Will Message, error: {} " + e.getMessage());
                 throw e;
             }
-        } else if (connectCommand.getWillMessage() != null && !connectCommand.getWillMessage().isEmpty()) {
+        } else if (connectCommand.getWillMessage() != null && !(connectCommand.getWillMessage().length == 0)) {
             //seems somebody like to create a will message without a topic
             Logger.debug("option -wt is missing if a will message is configured - command was: {} ", connectCommand.toString());
         }
@@ -223,11 +209,11 @@ abstract class AbstractMqttClientExecutor {
         if (connectCommand.getUser() != null && connectCommand.getPassword() != null) {
             connectBuilder.simpleAuth()
                     .username(connectCommand.getUser())
-                    .password(connectCommand.getPassword().getBytes(StandardCharsets.UTF_8))
+                    .password(connectCommand.getPassword())
                     .applySimpleAuth();
         } else if (connectCommand.getPassword() != null) {
             connectBuilder.simpleAuth()
-                    .password(connectCommand.getPassword().getBytes(StandardCharsets.UTF_8))
+                    .password(connectCommand.getPassword())
                     .applySimpleAuth();
         } else if (connectCommand.getUser() != null) {
             connectBuilder.simpleAuth()
