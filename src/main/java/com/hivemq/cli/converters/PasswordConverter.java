b11593cc27c5415250717fd13dbf22ee426c558d
Till Seeberger <seebergertill@gmail.com>
2019-08-08 17:53:46 +0200

    implement password reading from stdin

---
+++ src/main/java/com/hivemq/cli/converters/PasswordConverter.java
@@ -0,0 +1,18 @@
+package com.hivemq.cli.converters;
+
+import com.hivemq.cli.utils.PasswordUtils;
+import org.jetbrains.annotations.NotNull;
+import picocli.CommandLine;
+
+import java.nio.ByteBuffer;
+
+public class PasswordConverter implements CommandLine.ITypeConverter<ByteBuffer> {
+
+    @Override
+    public ByteBuffer convert(@NotNull String s) throws Exception {
+        if (s.isEmpty() || s.charAt(0) == '$') {
+            s = new String(PasswordUtils.readPassword("Please enter the authentication password:"));
+        }
+        return new ByteBufferConverter().convert(s);
+    }
+}
