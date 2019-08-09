6a4eb63525ef62c7386ae282332be5dd18a0f9da
Till Seeberger <seebergertill@gmail.com>
2019-08-09 13:36:52 +0200

    nullable

--- src/main/java/com/hivemq/cli/converters/PasswordConverter.java
+++ src/main/java/com/hivemq/cli/converters/PasswordConverter.java
@@ -2,6 +2,7 @@ package com.hivemq.cli.converters;
 
 import com.hivemq.cli.utils.PasswordUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import picocli.CommandLine;
 
 import java.nio.ByteBuffer;
@@ -9,7 +10,7 @@ import java.nio.ByteBuffer;
 public class PasswordConverter implements CommandLine.ITypeConverter<ByteBuffer> {
 
     @Override
-    public ByteBuffer convert(@NotNull String s) throws Exception {
+    public ByteBuffer convert(@Nullable String s) throws Exception {
         if (s.isEmpty() || s.charAt(0) == '$') {
             s = new String(PasswordUtils.readPassword("Please enter the authentication password:"));
         }
