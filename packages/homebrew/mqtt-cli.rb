ff05bb63a366f5da0d4b579f019598961788ad22
Till Seeberger <seebergertill@gmail.com>
2019-09-24 17:22:10 +0200

    automatically generate release on merge into master

--- packages/homebrew/mqtt-cli.rb
+++ packages/homebrew/mqtt-cli.rb
@@ -1,14 +1,14 @@
-class HivemqCli < Formula
+class MqttCli < Formula
   desc "@@description@@"
   homepage "https://www.hivemq.com"
-  url "https://www.hivemq.com/tests/@@filename@@"
+  url "https://github.com/hivemq/mqtt-cli/releases/download/v@@version@@/@@filename@@"
   sha256 "@@shasum@@"
   depends_on :java => "1.8+"
 
   def install
-    inreplace "brew/mqtt-cli", "##PREFIX##", "#{prefix}"
-    prefix.install "mqtt-cli.jar"
-    bin.install "brew/mqtt-cli"
+    inreplace "brew/mqtt", "##PREFIX##", "#{prefix}/mqtt-cli-@@version@@.jar"
+    prefix.install "mqtt-cli-@@version@@.jar"
+    bin.install "brew/mqtt"
   end
 
   test do
---
+++ packages/windows/README.txt
@@ -0,0 +1,4 @@
+To execute MQTT CLI simply open the Windows Command Prompt with ⊞Win + R and execute cmd.
+Navigate into the extracted MQTT CLI folder and execute the mqtt-cli.exe command.
+
+To quick start a MQTT CLI shell simply double-click the mqtt-cli-shell.cmd file.
