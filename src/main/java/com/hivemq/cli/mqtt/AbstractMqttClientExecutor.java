30d742eb431d07e40622ec38b61c4beae81877e8
Till Seeberger <seebergertill@gmail.com>
2019-10-09 17:29:23 +0200

    add NotNull annotation

--- src/main/java/com/hivemq/cli/mqtt/AbstractMqttClientExecutor.java
+++ src/main/java/com/hivemq/cli/mqtt/AbstractMqttClientExecutor.java
@@ -53,7 +53,7 @@ import java.util.Map;
 
 abstract class AbstractMqttClientExecutor {
 
-    private static final Map<String, ClientData> clientKeyToClientData = new HashMap<>();
+    @NotNull private static final Map<String, ClientData> clientKeyToClientData = new HashMap<>();
 
 
     abstract void mqtt5Connect(final @NotNull Mqtt5BlockingClient client, final @NotNull Mqtt5Connect connectMessage, final @NotNull Connect connect);
@@ -413,7 +413,7 @@ abstract class AbstractMqttClientExecutor {
         return null;
     }
 
-    public static Map<String, ClientData> getClientDataMap() {
+    public @NotNull static Map<String, ClientData> getClientDataMap() {
         return clientKeyToClientData;
     }
 
