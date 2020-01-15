86c4ec7332b28599995f738f1283a88f269ce4d7
Till Seeberger <seebergertill@gmail.com>
2020-01-15 12:26:32 +0100

    use single class imports

--- src/main/java/com/hivemq/cli/utils/JsonUtils.java
+++ src/main/java/com/hivemq/cli/utils/JsonUtils.java
@@ -16,7 +16,11 @@
  */
 package com.hivemq.cli.utils;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import org.jetbrains.annotations.NotNull;
 
 public class JsonUtils {
