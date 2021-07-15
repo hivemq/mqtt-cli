d52f13336683bb1c895e1f5674eabc3a0ca1dd32
gitseti <till.seeberger@hivemq.com>
2021-07-15 16:02:31 +0200

    Migrate buildscript DSL to kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,26 @@
 rootProject.name = "mqtt-cli"
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+
+    plugins {
+        id("com.github.johnrengelman.shadow") version "${extra["plugin.shadow.version"]}"
+        id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
+        id("org.gradle.crypto.checksum") version "${extra["plugin.checksum.version"]}"
+        id("nebula.ospackage") version "${extra["plugin.ospackage.version"]}"
+        id("edu.sc.seis.launch4j") version "${extra["plugin.launch4j.version"]}"
+        id("com.palantir.graal") version "${extra["plugin.graal.version"]}"
+        id("de.thetaphi.forbiddenapis") version "${extra["plugin.forbiddenapis.version"]}"
+        id("com.github.breadmoirai.github-release") version "${extra["plugin.github-release.version"]}"
+        id("org.ajoberstar.git-publish") version "${extra["plugin.git-publish.version"]}"
+        id("org.owasp.dependencycheck") version "${extra["plugin.dependencycheck.version"]}"
+        id("com.github.ben-manes.versions") version "${extra["plugin.ben-manes.versions.version"]}"
+        id("org.openapi.generator") version "${extra["plugin.openapi.generator.version"]}"
+    }
+}
+
 if (file("../plugins").exists()) {
     includeBuild("../plugins")
 }
