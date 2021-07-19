5dff5caa02d5fd103c22ad1edfa6d2c8eb03f8e5
gitseti <till.seeberger@hivemq.com>
2021-07-19 13:40:40 +0200

    build -> Finish kts migration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,9 @@
 rootProject.name = "mqtt-cli"
 
 pluginManagement {
-    repositories {
-        gradlePluginPortal()
-    }
-
     plugins {
         id("com.github.johnrengelman.shadow") version "${extra["plugin.shadow.version"]}"
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-        id("org.gradle.crypto.checksum") version "${extra["plugin.checksum.version"]}"
         id("nebula.ospackage") version "${extra["plugin.ospackage.version"]}"
         id("edu.sc.seis.launch4j") version "${extra["plugin.launch4j.version"]}"
         id("com.palantir.graal") version "${extra["plugin.graal.version"]}"
@@ -18,12 +13,14 @@ pluginManagement {
         id("org.owasp.dependencycheck") version "${extra["plugin.dependencycheck.version"]}"
         id("com.github.ben-manes.versions") version "${extra["plugin.ben-manes.versions.version"]}"
         id("org.openapi.generator") version "${extra["plugin.openapi.generator.version"]}"
+        id("com.google.cloud.tools.jib") version "${extra["plugin.jib.version"]}"
+    }
+
+    if (file("../plugins").exists()) {
+        includeBuild("../plugins")
     }
 }
 
-if (file("../plugins").exists()) {
-    includeBuild("../plugins")
-}
 if (file("../hivemq-enterprise").exists()) {
     includeBuild("../hivemq-enterprise")
 }
