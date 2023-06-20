7e1d66a1e4285311b84c5cc2a4d96527acc2c423
Silvio Giebl <silvio.giebl@hivemq.com>
2023-06-20 11:31:40 +0200

    Update downloadLicenses aliases

--- build.gradle.kts
+++ build.gradle.kts
@@ -345,7 +345,7 @@ license {
 
 downloadLicenses {
     aliases = mapOf(
-        license("Apache License, Version 2.0", "https://opensource.org/licenses/Apache-2.0") to listOf(
+        license("Apache-2.0", "https://spdx.org/licenses/Apache-2.0.html") to listOf(
             "Apache 2",
             "Apache 2.0",
             "Apache-2.0",
@@ -356,25 +356,26 @@ downloadLicenses {
             "Apache License Version 2.0",
             "Apache License, Version 2.0",
             "Apache License, version 2.0",
-            "The Apache License, Version 2.0",
             "Apache Software License - Version 2.0",
             "Apache Software License, version 2.0",
+            "The Apache License, Version 2.0",
             "The Apache Software License, Version 2.0",
-            "The Apache Software License, version 2.0"
+            "The Apache Software License, version 2.0",
         ),
-        license("MIT License", "https://opensource.org/licenses/MIT") to listOf(
+        license("MIT", "https://spdx.org/licenses/MIT.html") to listOf(
             "MIT License",
             "MIT license",
             "The MIT License",
-            "The MIT License (MIT)"
+            "The MIT License (MIT)",
+            "SPDX-License-Identifier: MIT",
         ),
-        license("CDDL, Version 1.0", "https://opensource.org/licenses/CDDL-1.0") to listOf(
+        license("CDDL-1.0", "https://spdx.org/licenses/CDDL-1.0.html") to listOf(
             "CDDL, Version 1.0",
             "Common Development and Distribution License 1.0",
             "COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Version 1.0",
-            license("CDDL", "https://glassfish.dev.java.net/public/CDDLv1.0.html")
+            license("CDDL", "https://glassfish.dev.java.net/public/CDDLv1.0.html"),
         ),
-        license("CDDL, Version 1.1", "https://oss.oracle.com/licenses/CDDL+GPL-1.1") to listOf(
+        license("CDDL-1.1", "https://spdx.org/licenses/CDDL-1.1.html") to listOf(
             "CDDL 1.1",
             "CDDL, Version 1.1",
             "Common Development And Distribution License 1.1",
@@ -382,59 +383,48 @@ downloadLicenses {
             "CDDL + GPLv2 with classpath exception",
             "Dual license consisting of the CDDL v1.1 and GPL v2",
             "CDDL or GPLv2 with exceptions",
-            "CDDL/GPLv2+CE"
+            "CDDL/GPLv2+CE",
         ),
-        license("LGPL, Version 2.0", "https://opensource.org/licenses/LGPL-2.0") to listOf(
-            "LGPL, Version 2.0",
-            "GNU General Public License, version 2"
-        ),
-        license("LGPL, Version 2.1", "https://opensource.org/licenses/LGPL-2.1") to listOf(
-            "LGPL, Version 2.1",
-            "LGPL, version 2.1",
-            "GNU Lesser General Public License version 2.1 (LGPLv2.1)",
-            license("GNU Lesser General Public License", "http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html")
-        ),
-        license("LGPL, Version 3.0", "https://opensource.org/licenses/LGPL-3.0") to listOf(
-            "LGPL, Version 3.0",
-            "Lesser General Public License, version 3 or greater"
-        ),
-        license("EPL, Version 1.0", "https://opensource.org/licenses/EPL-1.0") to listOf(
+        license("EPL-1.0", "https://spdx.org/licenses/EPL-1.0.html") to listOf(
             "EPL, Version 1.0",
             "Eclipse Public License - v 1.0",
             "Eclipse Public License - Version 1.0",
-            license("Eclipse Public License", "http://www.eclipse.org/legal/epl-v10.html")
+            license("Eclipse Public License", "http://www.eclipse.org/legal/epl-v10.html"),
         ),
-        license("EPL, Version 2.0", "https://opensource.org/licenses/EPL-2.0") to listOf(
+        license("EPL-2.0", "https://spdx.org/licenses/EPL-2.0.html") to listOf(
             "EPL 2.0",
-            "EPL, Version 2.0"
+            "EPL, Version 2.0",
         ),
-        license("EDL, Version 1.0", "https://www.eclipse.org/org/documents/edl-v10.php") to listOf(
+        license("BSD-3-Clause", "https://www.eclipse.org/org/documents/edl-v10.php") to listOf(
             "EDL 1.0",
             "EDL, Version 1.0",
-            "Eclipse Distribution License - v 1.0"
+            "Eclipse Distribution License - v 1.0",
         ),
-        license("BSD 3-Clause License", "https://opensource.org/licenses/BSD-3-Clause") to listOf(
+        license("BSD-2-Clause", "https://spdx.org/licenses/BSD-2-Clause.html") to listOf(
+            "BSD 2-Clause License",
+        ),
+        license("BSD-3-Clause", "https://spdx.org/licenses/BSD-3-Clause.html") to listOf(
             "BSD 3-clause",
             "BSD-3-Clause",
             "BSD 3-Clause License",
             "3-Clause BSD License",
             "New BSD License",
+            license("The BSD License", "https://opensource.org/licenses/BSD-3-Clause"),
             license("BSD", "http://asm.ow2.org/license.html"),
             license("BSD", "http://asm.objectweb.org/license.html"),
-            license("BSD", "LICENSE.txt")
+            license("BSD", "http://www.opensource.org/licenses/bsd-license.php"),
+            license("BSD", "LICENSE.txt"),
         ),
-        license("Bouncy Castle License", "https://www.bouncycastle.org/licence.html") to listOf(
-            "Bouncy Castle Licence"
+        license("MIT", "https://www.bouncycastle.org/licence.html") to listOf(
+            "Bouncy Castle Licence",
         ),
-        license("W3C License", "https://opensource.org/licenses/W3C") to listOf(
-            "W3C License",
-            "W3C Software Copyright Notice and License",
-            "The W3C Software License"
+        license("W3C-19980720", "https://spdx.org/licenses/W3C-19980720.html") to listOf(
+            license("W3C Software Copyright Notice and License", "http://www.w3.org/Consortium/Legal/copyright-software-19980720"),
+            license("The W3C Software License", "http://www.w3.org/Consortium/Legal/copyright-software-19980720"),
         ),
-        license("CC0", "https://creativecommons.org/publicdomain/zero/1.0/") to listOf(
+        license("CC0-1.0", "https://spdx.org/licenses/CC0-1.0.html") to listOf(
             "CC0",
-            "Public Domain"
-        )
+        ),
     )
 
     dependencyConfiguration = "runtimeClasspath"
