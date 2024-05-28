39a8cbb6f3de7797187a38c1e25c7c68a09d4bd5
Silvio Giebl <silvio.giebl@hivemq.com>
2024-05-28 11:14:39 +0200

    Update version to 4.29.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,8 @@ plugins {
 
 /* ******************** metadata ******************** */
 
-val prevVersion = "4.28.0"
-version = "4.28.1"
+val prevVersion = "4.28.1"
+version = "4.29.0"
 group = "com.hivemq"
 description = "MQTT CLI is a tool that provides a feature rich command line interface for connecting, " + //
         "publishing, subscribing, unsubscribing and disconnecting " + //
