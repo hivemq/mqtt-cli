93099afa259a4eb8b67c37ef941e8820b65e32f6
Carmine Fabrizio <carmine.fabrizio@hivemq.com>
2026-01-27 11:05:23 +0100

    Update version to 4.48.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,8 @@ plugins {
 
 /* ******************** metadata ******************** */
 
-val prevVersion = "4.46.0"
-version = "4.47.0"
+val prevVersion = "4.47.0"
+version = "4.48.0"
 group = "com.hivemq"
 description = "MQTT CLI is a tool that provides a feature rich command line interface for connecting, " + //
         "publishing, subscribing, unsubscribing and disconnecting " + //
