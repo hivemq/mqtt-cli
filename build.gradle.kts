c085679ee215fdd0c0c039438b834368add03286
Yonathan Mengesha <yonathan.mengesha@hivemq.com>
2026-06-30 17:11:16 +0200

    Update version to 4.53.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,8 +29,8 @@ plugins {
 
 /* ******************** metadata ******************** */
 
-val prevVersion = "4.51.0"
-version = "4.52.0"
+val prevVersion = "4.52.0"
+version = "4.53.0"
 group = "com.hivemq"
 description = "MQTT CLI is a tool that provides a feature rich command line interface for connecting, " + //
         "publishing, subscribing, unsubscribing and disconnecting " + //
