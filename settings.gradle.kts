5d9548276bc24492e8d13d61b98ade7c45f2e9d7
Yonathan Negussie Mengesha <yonathan.neg@gmail.com>
2026-09-11 04:56:14 +0200

    TEMP PLT-1261: includeBuild forked gradle-oci for ECR blob-HEAD fix validation
    Validation only (SgtSilvio/gradle-oci#154). Revert before merge.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,3 +7,8 @@ pluginManagement {
 }
 
 rootProject.name = "mqtt-cli"
+// PLT-1261 validation: build the OCI plugin from the forked gradle-oci sibling (ECR Public
+// blob-HEAD fix, SgtSilvio/gradle-oci#154) when present. Revert before merge.
+if (file("../gradle-oci").exists()) {
+    includeBuild("../gradle-oci")
+}
